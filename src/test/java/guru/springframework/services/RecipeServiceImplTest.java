0b571a373edb9bc91b70405cae903399354e0912
Dirk Haertel <dirk.hartel@absa.co.za>
2018-03-05 14:53:58 +0200

    finished 126

---
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -0,0 +1,48 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by dirk on 2018/03/05.
+ **/
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+        HashSet recipesData = new HashSet();
+        recipesData.add(new Recipe());
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+}
